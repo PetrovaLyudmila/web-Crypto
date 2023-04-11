453cdbb6f2d8c5bd8b34bcea1ef454a815f47e08
Lyudmila Petrova <111292349+PetrovaLyudmila@users.noreply.github.com>
2023-04-11 09:37:14 +0300

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,1 +1,8 @@
 exports.SECRET = '987176f76787d74a86aba62b1ce21b3aedae9cbf';
+
+exports.paymentMethodsMap = {
+    "crypto-wallet": "Crypto Wallet",
+    "credit-card": "Credit Card",
+    "debit-card": "Debit Card",
+    "paypal": "PayPal",
+}
